6d9e57b15bcbf3aad070ee3d4fc67e5aca6ed448
M4TT6NS <mitvin1947@students.uplifteducation.org>
2025-07-29 10:39:16 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+import pygame
+import time
+import random
+
+WIDTH, HEIGHT = 1000, 800
+WIN = pygame.display.set.mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Python Game")
+
+PLAYER_WIDTH = 40
+PLAYER_HEIGHT = 60
+
+def draw(player):
+   WIN.blit(BG, (0, 0)
+
+    pygame.draw.rect(WIN, "black", player)
+
+   pygame.display.update()
+
+def main():
+  run = True
+
+  player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HIEGHT)
+
+  while run:
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        run = False
+        break
+
+  draw(player)
+
+  pygame.quit()
+
+if __name__ == "__main__":
+  main()
